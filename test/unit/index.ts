47a4ce2d4f1b568778c9f33b52e894577a9deb72
pachet <pachet@burninggarden.com>
2020-12-06 16:52:28 -0500

    Fix some linter errors

--- test/unit/index.ts
+++ test/unit/index.ts
@@ -3,6 +3,7 @@ import Server from 'server';
 
 describe('package.json', () => {
 	it('contains expected package name', () => {
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
 		const manifest = require('../../../package.json');
 
 		expect(manifest.name).toStrictEqual('@9db/node-server');
