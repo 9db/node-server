61ca3d9134dfc88703a30ab340286ed366db3a57
pachet <pachet@burninggarden.com>
2020-12-08 11:21:40 -0500

    Add missing JsonNotFound endpoint

---
+++ test/unit/endpoint/json/not-found.ts
@@ -0,0 +1,51 @@
+import HTTP from 'http';
+
+import HttpHeader from 'http/enum/header';
+import StatusCode from 'http/enum/status-code';
+import ContentType from 'http/enum/content-type';
+import closeServer from 'http/utility/close-server';
+import fetchJson from 'http/utility/fetch-json';
+import JsonNotFoundRoute from 'route/json/not-found';
+import JsonNotFoundEndpoint from 'endpoint/json/not-found';
+
+describe('JsonNotFoundEndpoint', () => {
+	describe('process()', () => {
+		const port = 4482;
+
+		let server!: HTTP.Server;
+
+		beforeEach(() => {
+			server = HTTP.createServer((request, response) => {
+				const route = new JsonNotFoundRoute();
+
+				const endpoint = new JsonNotFoundEndpoint(
+					request,
+					response,
+					route
+				);
+
+				endpoint.serve();
+			});
+
+			server.listen(port);
+		});
+
+		afterEach((done) => {
+			closeServer(server).then(done);
+		});
+
+		it('returns expected response data', async () => {
+			const result = await fetchJson(`http://localhost:${port}`);
+
+			expect(result.body).toStrictEqual({
+				message: 'File not found'
+			});
+
+			expect(result.status_code).toStrictEqual(StatusCode.FILE_NOT_FOUND);
+
+			expect(result.headers).toMatchObject({
+				[HttpHeader.CONTENT_TYPE]: ContentType.JSON
+			});
+		});
+	});
+});
