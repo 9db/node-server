f7940aef751639738163a3fc47abb8ec4c305a9b
pachet <pachet@burninggarden.com>
2020-12-08 10:29:05 -0500

    Add missing Route unit test

--- test/unit/route.ts
+++ test/unit/route.ts
@@ -1,8 +1,12 @@
+import HTTP from 'http';
+
 import Route from 'route';
 import HttpMethod from 'http/enum/method';
 import ContentType from 'http/enum/content-type';
 import MockEndpoint from 'test/mock/endpoint';
+import RouteInterface from 'interface/route';
 import buildMockRequest from 'test/utility/build-mock-request';
+import buildMockResponse from 'test/utility/build-mock-response';
 
 describe('Route', () => {
 	describe('accepts()', () => {
@@ -59,6 +63,46 @@ describe('Route', () => {
 			it('returns true', () => {
 				expect(route.accepts(request)).toBe(true);
 			});
+		});
+	});
+
+	describe('serve()', () => {
+		it('delegates to the supplied endpoint', () => {
+			expect.assertions(4);
+
+			const request = buildMockRequest('/', HttpMethod.GET, ContentType.TEXT);
+			const response = buildMockResponse();
+
+			class ThrowawayEndpoint extends MockEndpoint {
+				public constructor(
+					supplied_request: HTTP.IncomingMessage,
+					supplied_response: HTTP.ServerResponse,
+					supplied_route: RouteInterface
+				) {
+					expect(supplied_request).toStrictEqual(request);
+					expect(supplied_response).toStrictEqual(response);
+					expect(supplied_route).toStrictEqual(route);
+
+					super(supplied_request, supplied_response, supplied_route);
+				}
+			}
+
+			const route = new Route(
+				ContentType.TEXT,
+				HttpMethod.GET,
+				'/foo',
+				ThrowawayEndpoint
+			);
+
+			const serve_spy = jest.spyOn(ThrowawayEndpoint.prototype, 'serve');
+
+			serve_spy.mockImplementation(() => {
+				return undefined;
+			});
+
+			route.serve(request, response);
+
+			expect(serve_spy).toHaveBeenCalled();
 		});
 	});
 });
