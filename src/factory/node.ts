5e7b0e6b9ec6abd3bf4a0c1ca2221d7fd5821ab2
pachet <pachet@burninggarden.com>
2020-12-08 13:42:22 -0500

    Specify more types on Node

--- src/factory/node.ts
+++ src/factory/node.ts
@@ -4,12 +4,21 @@ function randomKey(): string {
 	return Math.random().toString(16).slice(2);
 }
 
+function randomCreator(): string {
+	const key = randomKey();
+
+	return `https://9db.org/public/account/${key}`;
+}
+
 class NodeFactory {
 	public static create(node?: Partial<Node>): Node {
 		return {
 			namespace_key: randomKey(),
 			type_key: randomKey(),
 			key: randomKey(),
+			creator: randomCreator(),
+			created_at: Date.now(),
+			updated_at: Date.now(),
 			...node,
 		};
 	}
--- src/type/node.ts
+++ src/type/node.ts
@@ -4,6 +4,9 @@ interface Node {
 	readonly namespace_key: string;
 	readonly type_key: string;
 	readonly key: string;
+	readonly creator: string;
+	readonly created_at: number;
+	readonly updated_at: number;
 	readonly [key: string]: FieldValue;
 }
 
