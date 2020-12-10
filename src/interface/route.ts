5b01a24dc88117eb0283903e06ac808a318623d0
pachet <pachet@burninggarden.com>
2020-12-10 08:19:13 -0500

    Remove unnecessary methods from route interface

--- src/interface/route.ts
+++ src/interface/route.ts
@@ -1,19 +1,8 @@
-import HTTP from 'http';
-
-import Repository from 'repository';
 import HttpMethod from 'http/enum/method';
 import StatusCode from 'http/enum/status-code';
 import ContentType from 'http/enum/content-type';
 
 interface Route {
-	accepts(request: HTTP.IncomingMessage): boolean;
-
-	serve(
-		request: HTTP.IncomingMessage,
-		response: HTTP.ServerResponse,
-		repository: Repository
-	): void;
-
 	getContentType(): ContentType;
 	getMethod(): HttpMethod;
 	getSuccessfulStatusCode(): StatusCode;
